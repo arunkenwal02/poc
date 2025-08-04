777df07f4b077b4f2b6016fd42ccb1a8c5ab769b
Arun Kenwal <arunkenwal@Aruns-MacBook-Air.local>
2025-08-04 22:10:44 +0530

    checking response

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import requests
+from urllib.parse import urlparse
+import re
 
 app = FastAPI(
     title='Code validator POC', 
@@ -6,6 +9,22 @@ app = FastAPI(
 )
 
 
-@app.post('/fetchpushhistory')
-def fetch_last_two_push(url:str):
-    return url
+@app.post("/fetchpushhistory")
+def get_latest_push_events(repo_url: str = Query(...)):
+    match = re.match(r"https://github.com/([^/]+)/([^/]+)(?:\.git)?", repo_url)
+    if match:
+        owner, repo_name_candidate = match.groups()
+        repo_name = repo_name_candidate[:-4] if repo_name_candidate.endswith('.git') else repo_name_candidate
+    else:
+        return {"error": "Invalid GitHub repository URL format"}
+
+    api_url = f"https://api.github.com/repos/{owner}/{repo_name}/events"
+
+    response = requests.get(api_url)
+
+    response.raise_for_status()
+    return response.json()
+
+
+    events = response.json()
+    return events[:2]
